9ca404f358ab9d35af1283027a52c7dd502592d9
runvincentrun <jhh-vincent@hotmail.com>
2021-12-25 21:10:53 +0800

    day3

---
+++ day03/path内置模块.js
@@ -0,0 +1,7 @@
+const path = require('path');
+// console.log(path.extname(__filename)); 文件后缀名
+// console.log(path.basename(__filename)) // 文件名
+// console.log(path.dirname(__filename));  文件夹名字
+// console.log(__dirname); // 当前文件夹
+// console.log(__filename); // 当前文件名
+// console.log(this);
